a395ee279917597f9ac16a73e72f9153a000295c
Tze Sheng <ngtzesheng123@gmail.com>
2023-01-27 17:35:43 +0800

    [Code Reformat]

--- Android/Code/app/src/main/java/com/example/code/ui/screens/bluetooth/BluetoothScreen.kt
+++ Android/Code/app/src/main/java/com/example/code/ui/screens/bluetooth/BluetoothScreen.kt
@@ -1,10 +1,22 @@
 package com.example.code.ui.screens.bluetooth
 
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
 fun BluetoothScreen() {
-    Text(text = "Bluetooth Screen")
+    Row(
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier.fillMaxWidth(1f)
+    ) {
+        Column {
+            Text(text = "Choose a device to pair with")
+        }
+        Column {
+            Text(text = "Available Bluetooth Devices")
+        }
+    }
 }
 
