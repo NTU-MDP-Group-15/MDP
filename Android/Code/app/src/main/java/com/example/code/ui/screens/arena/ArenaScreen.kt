e3c31ac212d005f1e4cc8a82097d29dd2920f802
seahyh <yeehao_seah@hotmail.com>
2023-01-31 23:36:51 +0800

    Arena and Debug UI

--- Android/Code/app/src/main/java/com/example/code/ui/screens/arena/ArenaScreen.kt
+++ Android/Code/app/src/main/java/com/example/code/ui/screens/arena/ArenaScreen.kt
@@ -1,10 +1,41 @@
 package com.example.code.ui.screens.arena
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ArenaScreen() {
-    Text(text = "Arena Screen")
+    Column() {
+        Text(text = "Arena Screen")
+        Primes()
+    }
+}
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun Primes() {
+    LazyVerticalGrid(
+        modifier = Modifier // 1
+            .fillMaxWidth(0.4F)
+            .background(Color(0xFFE53935))
+            .padding(8.dp),
+        columns = GridCells.Fixed(10), // 2
+    ) {
+        items(count = 100) { // 3
+            Box(
+                Modifier // 4
+                    .aspectRatio(1f)
+                    .padding(1.dp)
+                    .background(Color.Cyan)
+            )
+        }
+    }
 }
 
--- Android/Code/app/src/main/java/com/example/code/ui/screens/debug/DebugScreen.kt
+++ Android/Code/app/src/main/java/com/example/code/ui/screens/debug/DebugScreen.kt
@@ -1,10 +1,80 @@
 package com.example.code.ui.screens.debug
 
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 @Composable
 fun DebugScreen() {
-    Text(text = "Debug Screen")
+    Column() {
+        Row (horizontalArrangement = Arrangement.SpaceEvenly,
+            modifier = Modifier.fillMaxWidth(1f))
+        {
+            Text(text = "Connected Devices: ", fontSize=50.sp)
+        }
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxWidth(0.9f)
+                .padding(10.dp)
+        ) {
+            Column {
+                Text(text = "Send To Remote", fontSize = 30.sp)
+                Card(modifier = Modifier
+                    .fillMaxHeight(0.2F)
+                    .border(width = 2.dp, color = Color.Black)) {
+                    LabelAndPlaceHolder()
+                }
+                Row(horizontalArrangement=Arrangement.SpaceBetween,
+                modifier=Modifier.fillMaxWidth(0.273f)) {
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text="Send")
+                    }
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text="Clear")
+                    }
+                }
+
+            }
+            Column {
+                Text(text = "Command Log", fontSize = 30.sp)
+                Card(modifier = Modifier
+                    .fillMaxHeight(0.5F)
+                    .fillMaxWidth(0.5f)
+                    .border(width = 2.dp, color = Color.Black)) {
+
+                }
+            }
+        }
+    }
 }
+
+@Composable
+fun LabelAndPlaceHolder() {
+    var text by remember { mutableStateOf(TextFieldValue("")) }
+    TextField(
+        value = text,
+        onValueChange = {
+            text = it
+        },
+        label = { Text(text = "Your Messages") },
+        placeholder = { Text(text = "Enter Message Here") },
+    )
+}
+
 
