cde2d1ff3c375de26ef578f38da41f77dd792a72
Tze Sheng <ngtzesheng123@gmail.com>
2023-02-10 16:02:54 +0800

    code tidy up

--- Android/Code/app/src/main/java/com/example/code/ui/viewmodels/BluetoothViewModel.kt
+++ Android/Code/app/src/main/java/com/example/code/ui/viewmodels/BluetoothViewModel.kt
@@ -1,18 +1,6 @@
 package com.example.code.ui.viewmodels
 
-import android.annotation.SuppressLint
-import android.bluetooth.BluetoothAdapter
-import android.bluetooth.BluetoothAdapter.getDefaultAdapter
 import android.bluetooth.BluetoothDevice
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.os.Handler
-import android.os.Looper
-import android.util.Log
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.runtime.*
 import androidx.lifecycle.ViewModel
 import com.example.code.ui.states.BluetoothUiState
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -21,10 +9,6 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
 class BluetoothViewModel : ViewModel() {
-    // TODO: should only handle data, move others to BluetoothService
-    // Debugging
-    private val TAG: String? = "BluetoothViewModel"
-
     // Bluetooth UI State
     private val _uiState = MutableStateFlow(BluetoothUiState())
     val uiState: StateFlow<BluetoothUiState> = _uiState.asStateFlow()
