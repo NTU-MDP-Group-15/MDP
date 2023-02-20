a2576d56f42f33a4dd606130a15232e429a55a3f
Nicholas Png (XPS13Plus) <NicholasPng@XPS13Plus>
2023-02-21 01:40:49 +0800

    Removed unnecessary files

--- RPI/Algorithm/image_rec/build/lib/imagerec/main.py
+++
@@ -1,7 +0,0 @@
-import os
-
-img_path = '../test_labels'
-os.system(f'python predict.py {img_path}')
-for img in os.listdir(img_path):
-    img = os.path.join(img_path, img)
-    os.system(f'python infer.py {img}')
