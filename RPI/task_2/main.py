715ba617bd7db45a1303d7ad70ae1d6c041759c3
Nicholas Png (XPS13Plus) <NicholasPng@XPS13Plus>
2023-03-13 23:33:52 +0800

    REMOVED ALGO_INT

--- RPI/task_2/main.py
+++ RPI/task_2/main.py
@@ -2,61 +2,23 @@
 Filename: main.py
 Version: v3.1
 
-Starts all relevant threads required for MDP
-
-main.py 
-    workerThread:
-        - thread_proc: contains logic for data processing between
-                       different interfaces
-
-imgrecInterface.py 
-    workerThread:
-        - listener
-
-stmInterface.py
-    workerThread:
-        - listener
-        - sender
-
-btInterface.py 
-    workerThread:
-        - listener
-        - sender                 
+Start relevant interfaces/threads for Task 2               
 
 ! Updates (DDMMYY)
-200223 - Added logic for BT -> RPI -> STM
-210223 - Removed threading on MDPPi
-230223 - Logic for task A5
-         Updated logic for sending of pictures
-260223 - Updated to taskv1
-       - Added logic between ANDROID <-> RPI, RPI <-> ALGO
-270223 - Moved thread_proc to dataHandler.py
+130323 - Removed algo 
 
 
-Order of connection STM -> IMGREC -> ALGO -> BT
-
-How data should be moved:
-Step 1: RPI receives from Android the obstacle -> sends to algo laptop, ANDROID_IN -> ALGO_OUT
-Step 2: RPI receives from Algo the shortest path -> sends to Android & buffer instructions ALGO_IN -> ANDROID_OUT, STM_OUT
-Step 3: RPI waits for Android "Start" before sending buffered instructions to STM, ANDROID_IN -> None 
-Step 4: RPI sends instructions to STM
-     4a: waits for acknowledgement when sub instruction is done, and send the next sub instruction
-     4b: when all sub instructions (aka full instructions done), take photo for imgrec and sends to Android, IMGREC_IN -> ANDROID_OUT
-Repeat step 4.
-
+Order of connection STM -> IMGREC -> BT
 """
-import time
 import modules 
 
 class MDPPi:
     def __init__(self):
         self.stm_int = modules.STMInterface()
         self.bt_int = modules.BTServerInterface()          
-        self.algo_int = modules.AlgoServerInterface()
         self.im_int = modules.ImageRecInterface(no_of_pic=6)
         self.dh = modules.DataHandler(stm_int=self.stm_int,
                                       bt_int=self.bt_int,
-                                      algo_int=self.algo_int,
                                       im_int=self.im_int
                                      )
     def __call__(self):
@@ -70,18 +32,26 @@ class MDPPi:
         self.clean_close()
             
     def connect(self) -> bool:
-        print("[PI/INFO] Connecting devices")
-        stm_con = self.stm_int.connect()          # Connect stm first
+        print("[PI/INFO] CONNECTING STM")
+        stm_con = self.stm_int.connect()
+        if stm_con: print("[PI/INFO] STM CONNECTED")
+        else: print("[PI/INFO] STM NOT CONNECTED")
+        
+        print("[PI/INFO] CONNECTING IMAGE REC")
         im_con = self.im_int.connect()
-        algo_con = self.algo_int.connect()
+        if im_con: print("[PI/INFO] IMAGE REC CONNECTED")
+        else: print("[PI/INFO] IMAGE REC NOT CONNECTED")
+        
+        print("[PI/INFO] CONNECTING ANDROID")
         bt_con = self.bt_int.connect()
-        return (stm_con and im_con and algo_con and bt_con)
+        if bt_con: print("[PI/INFO] ANDROID CONNECTED") 
+        else: print("[PI/INFO] ANDROID NOT CONNECTED")
+        return (stm_con and im_con and bt_con)
         #return True
         
     def clean_close(self) -> None:
         self.stm_int.disconnect()
         self.im_int.disconnect()
-        self.algo_int.disconnect()
         self.bt_int.disconnect()
         
 if __name__ == '__main__':
