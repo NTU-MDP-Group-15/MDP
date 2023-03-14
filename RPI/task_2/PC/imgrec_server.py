cf7aeb8cba0bfaae6f924249337ee0846453eef2
Nicholas Png (XPS13Plus) <NicholasPng@XPS13Plus>
2023-03-15 01:16:38 +0800

    Added more models

--- RPI/task_2/PC/imgrec_server.py
+++ RPI/task_2/PC/imgrec_server.py
@@ -6,21 +6,19 @@ pip install yolov5 instead of ultralytics
 
 ! Updates (DDMMYY)
 010323 - Moved getting most occuring id from RPI to server
+150323 - Removed bulleyes recognition + logic for most_occuring_id was wrong...
 """
 import os
 import cv2
 import zmq
-import time
 import torch
 import imagezmq
 import numpy as np
 import socket
-import traceback
 
 # import imutils
 # import yolov5
-# from PIL import Image
-
+from PIL import Image
 
 MIN_CONFIDENCE_THRESHOLD = 0.65         # Change this to ensure no double results
 NON_RED_CONFIDENCE_THRESHOLD = 0.55
@@ -29,17 +27,18 @@ NMS_IOU = 0.55
 RPI_IP = "192.168.15.1"
 IMGREC_PORT = 12348
 
-IMG_ZMQ_IP = "192.168.15.69"
+IMG_ZMQ_IP = "192.168.15.59"
 IMG_ZMQ_PORT = 5555
 ID_ZMQ_IP = "192.168.15.1"
 ID_ZMQ_PORT = 5556
 
 
 #MODEL_PATH = os.path.join(".", "YOLOv5", "yolov5s.pt")     # ./bestv5.pt .\bestv5.pt
-MODEL_PATH = os.path.join(".", "bestv5.pt")     # ./bestv5.pt .\bestv5.pt
+# MODEL_PATH = os.path.join(".", "best.pt")     # ./bestv5.pt .\bestv5.pt
+MODEL_PATH = os.path.join(".", "T2_best_2.pt")     # ./bestv5.pt .\bestv5.pt
 YOLO_PATH = os.path.join(".","YOLOv5")
 
-NO_OF_PIC = 6
+NO_OF_PIC = 3
 
 CUR_DIR = os.path.dirname(os.path.abspath(__file__))
 IMG_DIR = os.path.join(CUR_DIR, 'static', "images")
@@ -73,7 +72,6 @@ class ImgRecServer:
         self.img_format = "jpg"
         
         self.model = self.load_model(model_path, yolo_path)
-        input("> ")
         
         self.image_hub = self.get_image_hub()
         self.id_pub = self.get_id_pub()
@@ -93,7 +91,7 @@ class ImgRecServer:
         return model
 
     def get_image_hub(self):
-            # imagezmq.ImageHub(open_port=self.zmq_address, REQ_REP=False)       # PUB/SUB
+        # return imagezmq.ImageHub(open_port=self.img_zmq_address, REQ_REP=False)       # PUB/SUB
         return imagezmq.ImageHub(open_port=self.img_zmq_address)
     
     def get_id_pub(self):
@@ -117,23 +115,23 @@ class ImgRecServer:
         
     def __call__(self):
         print("[IMGREC_S/INFO] Ready and listening for frames")
-        while True:
-            try:
-                (rpi_name, frame) = self.image_hub.recv_image()
-                self.image_hub.send_reply(b'OK')      # Required in REQ/REP
-                print("[IMGREC_S/INFO] Received frame")
-
+        try:
+            while True:
+                try:
+                    rpi_name, frame = self.image_hub.recv_image()
+                    self.image_hub.send_reply(b'OK')      # Required in REQ/REP
+                    cv2.waitKey(1)
+                    print("[IMGREC_S/INFO] Received frame")
+                except KeyboardInterrupt:
+                    self.id_array = list()
+                    continue
+                
                 #cv2.imshow(rpi_name, frame)
                 #cv2.waitKey(1)
                 
                 results = self.model(frame)
                 
                 results.save(save_dir=IMG_DIR)
-                # print("SAVING IMAGE")
-                # img_name = self.img_name.format(img_idx=self.img_idx, img_format=self.img_format)
-                # cv2.imwrite(os.path.join(IMG_DIR, img_name), results)
-                
-                # self.img_idx += 1
                 #results.show()            # show image with box
                 
                 pd = results.pandas().xyxy[0]
@@ -150,70 +148,64 @@ class ImgRecServer:
                     if len(filtered_list) == 0: 
                         most_occurring_id = 99
                     else:
-                        most_occurring_id = int(max(set(self.id_array), key=self.id_array.count))                    
-                    # self.c_sock.sendall(str(most_occurring_id).encode())
-                    self.id_pub.send_string(most_occurring_id)
+                        most_occurring_id = int(max(set(filtered_list), key=filtered_list.count))                    
+
+                    self.id_pub.send_string(str(most_occurring_id))
                     self.id_array = list()
                     
-            except KeyboardInterrupt:
-                print("[IMGREC_S/INFO] KeyboardInterrupt received")
-                break
-            
-            except:
-                traceback.print_exc()   
-                break  
+        except KeyboardInterrupt:
+            print("[IMGREC_S/INFO] KeyboardInterrupt received")
         cv2.destroyAllWindows()
         
     def get_highest_confidence_id(self, pd) -> int:
+        # remove rows with bulleyes id
+        i = pd[(pd["name"] == "0")].index
+        pd = pd.drop(i)
+        
         if len(pd) == 0: return 99
         else:
             highest_confidence = pd['confidence'].max()
+            
             for idx, row in pd.iterrows():    
                 if row['confidence'] == highest_confidence:
                     return int(row['name'])
                 
-    def save_processed_photo(self, frame):
-        pass
-    
-    '''
-    # old
-    def __call__(self):
+    def test(self):
         print("[IMGREC_S/INFO] Ready and listening for frames")
-        while True:
-            try:
-                (rpi_name, frame) = self.image_hub.recv_image()
-                self.image_hub.send_reply(b'OK')      # Required in REQ/REP
-                print("[IMGREC_S/INFO] Received frame")
-
-                # cv2.imshow(rpi_name, frame)
-                # cv2.waitKey(1)
-                
-                results = self.model(frame)
-                #results.show()            # show image with box
-                pd = results.pandas().xyxy[0]
-                id = self.process_data(pd)
-                self.c_sock.sendall(str(id).encode())
-                
-            except KeyboardInterrupt:
-                print("[IMGREC_S/INFO] KeyboardInterrupt received")
-                self.c_sock.sendall(b"disconnect")
-                break
-            except:
-                self.c_sock.sendall(b"disconnect")
-                traceback.print_exc()   
-                break  
-        cv2.destroyAllWindows()
-    
-    def process_data(self, pd)  -> int:
-        if len(pd)==0: 
-            self.id_array.append(99)
-        else:
-            highest_confidence = pd["confidence"].max()
-            for idx, row in pd.iterrows():    
-                if row['confidence'] == highest_confidence:
-                    self.id_array.append(row['name'])
-    '''
+        
+        im1 = Image.open(os.path.join(CUR_DIR, "test_images", "Bullseye.jpg"))
+        
+        results = self.model(im1)
+        #results.show()            # show image with box
+        
+        pd = results.pandas().xyxy[0]
+        
+        i = pd[(pd["name"] == "0")].index
+        print(i)
+        pd = pd.drop(i)
+        
+        for idx, row in pd.iterrows():    
+            print(row)
+            
+        print("len(pd): ", len(pd))
+        
+        # Get highest confidence ID based off all detection
+        #highest_conf_id = self.get_highest_confidence_id(pd)
+        
+        # Add id to array
+        # self.id_array.append(highest_conf_id)
+        
+        # filtered_list = list(filter((99).__ne__, self.id_array))
+        #     print(filtered_list)
+        #     if len(filtered_list) == 0: 
+        #         most_occurring_id = 99
+        #     else:
+        #         most_occurring_id = int(max(set(self.id_array), key=self.id_array.count))                    
+        #     # self.c_sock.sendall(str(most_occurring_id).encode())
+        #     self.id_pub.send_string(str(most_occurring_id))
+        #     self.id_array = list()
     
 if __name__=="__main__":
     im = ImgRecServer()
     im()
+    # im.test()
